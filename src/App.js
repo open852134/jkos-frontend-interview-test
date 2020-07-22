42e015463db376aab60dd8ac363d13134e45e62d
open852134 <open852134@gmail.com>
2020-07-22 17:35:38 +0800

    update picture ulr

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { Login } from "./pages";
 
 const FullLayout = styled.div`
   padding: 60px 40px;
+  min-height: 100vh;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 500px 500px;
+  background-position: 50% 150%;
+  background-image: url("./images/img_town.svg");
 `;
 
 const App = () => {
